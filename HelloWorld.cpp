f43d1646c49b68192bb2de83bf45c020de6e4722
guoxuter <guoxuter@gmail.com>
2022-09-17 20:57:17 +0800

    my first commmit

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cout << "Goodbye, 2019!" << endl;
+    cout << "Hello, 2020!" << endl;
+    cout << "Hello, Windows!" ;
+
+    return 0;
+}
